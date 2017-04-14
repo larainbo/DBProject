cc6b594ade7e2bd50d38f5b9f33b376c63acaef3
larainbo <lauren.rainbolt@yahoo.com>
2017-04-13 23:55:10 -0500

    Add files via upload

---
+++ SQL.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Student (
+StudentID INT PRIMARY KEY,
+StudentName CHAR(25) NOT NULL,
+StudentMajor Char(25) NOT NULL,
+)
+ENGINE=innoDB;
+
+CREATE TABLE Enrollment (
+StudentID INT,
+DeptCode CHAR(4),
+CourseNum INT,
+FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
+FOREIGN KEY (DeptCode)  REFERENCES Course(DeptCode),
+FOREIGN KEY (CourseNum) REFERENCES Course(CourseNum)
+)ENGINE=InnoDB;
